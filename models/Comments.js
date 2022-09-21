90018f67f18fcbe8f4d9bcd346a12bb3d8855f94
unknown <steveweed1979@gmail.com>
2022-09-20 21:58:16 -0500

    Working on handlebars

---
+++ models/Comments.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model { }
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        timestamps: true,
+        modelName: 'comments',
+    }
+);
+
+module.exports = Comments;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers/"));
+app.use(require("./controllers/blog-routes"));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on ${Port}`));
